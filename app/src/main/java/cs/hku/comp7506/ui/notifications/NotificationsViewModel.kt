20e9ad4c242c8a05be6f82c6e9d8ea6640409f67
jonathan pun <jpppun@gmail.com>
2022-04-04 16:11:39 +0800

    init

---
+++ app/src/main/java/cs/hku/comp7506/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package cs.hku.comp7506.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
